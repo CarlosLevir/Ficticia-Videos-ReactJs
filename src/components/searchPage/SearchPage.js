d147867714b40f2754a7bbf4a9025f4fb67db0cb
Carlos Levir <carloslevirfs@gmail.com>
2018-12-18 08:22:52 -0300

    Change of Props Value Passed

--- src/components/searchPage/SearchPage.js
+++ src/components/searchPage/SearchPage.js
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
 import ChannelVideosCard from '../channelVideosCard/ChannelVideosCard';
-import * as YoutubeService from '../../services/youtubeServices/YoutubeServices';
 
 class SearchPage extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            videos: [],
-        }
-    }
-
-    componentDidMount = () => {
-        YoutubeService.getSearchedVideos(this.props.keyword).then((response) => {
-            this.setState({
-                videos: response.data.items,
-            })
-        })
-    }
-    
     render() {
         return (
             <div>
                 <ChannelVideosCard
                 titlePage={'Resultados para: "' + this.props.keyword + '"'}
-                videos={this.state.videos}
+                videos={this.props.videosDataSearch}
                 showButton={"none"}
                 />
             </div>
